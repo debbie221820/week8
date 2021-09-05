61b4c8e03fba672772cc58e2a79a7dc0885561ad
debbie221820 <debbie221820@gmail.com>
2021-09-05 13:08:45 +0800

    Update 2021-09-05T05:08:42.375Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,27 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 1,
+//     spaceBetween: 30,
+//     breakpoints: {
+//       768:{
+//         slidesPerView: 3,
+//         spaceBetween: 30,
+//       },
+//     }
+// });
+
+/*{ <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script> }*/
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 3,
+//   centeredSlides: true,
+//   spaceBetween: 30,
+//   grabCursor: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+"use strict";
 //# sourceMappingURL=all.js.map
